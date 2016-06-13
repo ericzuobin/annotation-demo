ca2374462e2e74a403b992b18eb8cd7e1ce2224f
ericzuobin <ericzuobin@qq.com>
2016-06-13 11:58:46 +0800

    修改demo

--- src/main/java/net/zuobin/controller/TestController.java
+++ src/main/java/net/zuobin/controller/TestController.java
@@ -1,5 +1,7 @@
 package net.zuobin.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,11 +15,16 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/testmodel")
 public class TestController {
 
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @RequestMapping(value="/test1",method= RequestMethod.GET)
     public ModelAndView test(){
 
         ModelAndView modelAndView = new ModelAndView("/test/test");
         modelAndView.addObject("name", "xxx");
+
+        logger.error("test success");
+
         return modelAndView;
     }
 }
